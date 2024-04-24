dea59fb9bb8ff7eacbd8db6bd6cb1a7dbc2e4878
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-24 18:31:58 +0300

    bagging

--- Hyper-ParameterTuniningandEnsembles.R
+++ Hyper-ParameterTuniningandEnsembles.R
@@ -19,3 +19,26 @@ svm_random_search <- train(Outcome ~ .,           # Specify the formula for the 
 
 # Display the best model from random search
 print(svm_random_search)
+
+# Load necessary libraries
+library(caret)
+
+# Define the control parameters for model training
+ctrl <- trainControl(method = "cv",    # Use k-fold cross-validation
+                     number = 10)      # Specify the number of folds (e.g., 10-fold)
+
+# Train bagged decision tree model
+bagging_model <- train(Outcome ~ .,            # Specify the formula for the model
+                       data = pima_data,       # Specify the dataset
+                       method = "treebag",    # Specify the modeling method (bagged decision trees)
+                       trControl = ctrl)      # Specify the control parameters for cross-validation
+
+# Display the bagged decision tree model
+print(bagging_model)
+
+
+# Define the base learners
+base_learners <- c("glm", "rpart", "svmRadial")
+
+
+
