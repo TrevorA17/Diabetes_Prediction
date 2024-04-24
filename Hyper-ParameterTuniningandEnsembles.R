09eb06e360604187d0d57fc82bfc9c192d3c93ed
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-24 18:29:48 +0300

    random search

---
+++ Hyper-ParameterTuniningandEnsembles.R
@@ -0,0 +1,21 @@
+
+# Load necessary libraries
+library(caret)
+
+# Define the control parameters for model training
+ctrl <- trainControl(method = "cv",    # Use k-fold cross-validation
+                     number = 10)      # Specify the number of folds (e.g., 10-fold)
+
+# Define the search grid for random search
+random_grid <- expand.grid(C = c(0.01, 0.1, 1, 10, 100),   # Specify the range of values for the regularization parameter (C)
+                           kernel = c("linear", "radial")) # Specify the types of kernels
+
+# Perform random search for SVM
+svm_random_search <- train(Outcome ~ .,           # Specify the formula for the model
+                           data = pima_data,     # Specify the dataset
+                           method = "svmRadial", # Specify the modeling method (SVM with radial kernel)
+                           trControl = ctrl,     # Specify the control parameters for cross-validation
+                           tuneGrid = random_grid) # Use the defined search grid
+
+# Display the best model from random search
+print(svm_random_search)
